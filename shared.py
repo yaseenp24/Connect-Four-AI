3299ec4b31e8cc4945942c394d4d1e8f699dc864
yp <138333687+yaseenp24@users.noreply.github.com>
2025-09-08 23:20:08 -0700

    Update shared.py
    Added descriptions

--- shared.py
+++ shared.py
@@ -5,6 +5,7 @@ def make_board(game_state):
     This function makes the board by inputting
     R's and Y's in places where a move has been 
     made, or .'s in a place that hasn't been made
+    it allows the board to know whats been played
     '''
     for row in range(connectfour.rows(game_state)):
             row_str = ""
